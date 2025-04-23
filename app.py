7cf6daed98215b43029f61a294bf480eb54f042e
epiz78 <epiz78@nate.com>
2025-04-23 17:57:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ def generate_video():
     tts = gTTS(text=summary_text, lang='ko')
     tts.save(audio_file)
 
-    ffmpeg_command = f'ffmpeg -y -loop 1 -i background.jpg -i {audio_file} -c:v libx264 -c:a aac -b:a 192k -shortest -pix_fmt yuv420p {video_file}'
+    # 배경 없는 기본 화면 (검은 배경으로)
+    ffmpeg_command = f'ffmpeg -f lavfi -i color=c=black:s=1280x720:d=10 -i {audio_file} -c:v libx264 -c:a aac -b:a 192k -shortest -pix_fmt yuv420p {video_file}'
     os.system(ffmpeg_command)
 
     response = send_file(video_file, as_attachment=True)
